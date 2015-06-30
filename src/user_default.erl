7b2934cbe84fa94ed2718f5ec07a4ae06892ed1f
konrads <sosnowski.konrad@gmail.com>
2015-06-30 11:34:22 +0100

    fixing dir structure, for apps & src dirs

--- src/user_default.erl
+++ src/user_default.erl
@@ -4,12 +4,14 @@
 
 -export([cr/1]).
 -export([cre/1]).
+-export([e/1]).
 
 help() ->
      shell_default:help(),
      io:format("** user extended commands **
 cr(Module)  -- compile and reload M
-cre(Module) -- compile, reload and eunit M"),
+cre(Module) -- compile, reload and eunit M
+e(Module)   -- eunit M"),
      true.
 
 cr(M) when is_atom(M) ->
@@ -17,7 +19,8 @@ cr(M) when is_atom(M) ->
     EbinDir = filename:dirname(filename:dirname(Src)) ++ "/ebin",
     {ok, Cwd} = file:get_cwd(),
     file:set_cwd(EbinDir),
-    SrcFromEbin = "../" ++ Src,
+    % src relative to ebin
+    SrcFromEbin = re:replace(Src, ".*src/", "../src/", [{return, list}]),
     try
         {ok, _} = compile:file(SrcFromEbin),
         code:purge(M),
@@ -25,10 +28,14 @@ cr(M) when is_atom(M) ->
     after file:set_cwd(Cwd)
     end.
 
-cre(M) when is_atom(M) ->
-    cr(M),
+e(M) when is_atom(M) ->
     eunit:test(M).
 
+cre(M) when is_atom(M) ->
+    cr(M),
+    e(M).
+
+%% internals
 src_file(M) ->
     case code:where_is_file(atom_to_list(M) ++ ".beam") of
         non_existing -> throw(non_existing);
